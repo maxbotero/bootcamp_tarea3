983ca4bb89f08844b85281ff4f5f6ee03f59f7d5
maxbotero <124061834+maxbotero@users.noreply.github.com>
2023-02-03 11:16:31 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-    var unaPersona = new Persona();
+    Persona unaPersona = new Persona();
     unaPersona.setEdad(50);
     System.out.println(unaPersona.getEdad());
 
