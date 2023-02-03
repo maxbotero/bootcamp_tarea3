abae4e0b81395b3a863380b0d185610163ff06a7
maxbotero <124061834+maxbotero@users.noreply.github.com>
2023-02-03 10:08:01 -0500

    Create Main.java
    Para practicar la encapsulación:
    - Crear clase Persona.
    - Crear variables las privadas edad, nombre y teléfono de la clase Persona.
    - Crear gets y sets de cada propiedad.
    - Crear un objeto persona en el main.
    - Utiliza los gets y sets para darle valores a las propiedades edad, nombre y telefono, por último muéstralas por consola.

---
+++ Main.java
@@ -0,0 +1,48 @@
+public class Main {
+    public static void main(String[] args) {
+
+    var unaPersona = new Persona();
+    unaPersona.setEdad(50);
+    System.out.println(unaPersona.getEdad());
+
+    unaPersona.setNombre("Pepe");
+    System.out.println(unaPersona.getNombre());
+
+    unaPersona.setTelefono("315245896");
+    System.out.println(unaPersona.getTelefono());
+
+    }
+
+}
+
+class Persona {
+    private int edad;
+    private String nombre;
+    private String telefono;
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getTelefono() {
+        return this.telefono;
+    }
+
+}
+
